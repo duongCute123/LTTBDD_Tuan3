6e03e52e8660a1331f24822c1137b0c70df38326
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2022-09-15 08:40:08 +0700

    add frame d

--- App.tsx
+++ App.tsx
@@ -3,10 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import Frame_a from './component/Frame1_a/frame_a';
 import Frame_b from './component/Frame_b/frame_b';
 import Frame_c from './component/Frame_c/frame_c';
+import Frame_d from './component/Frame_d/frame_d';
 export default function App() {
   return (
     <View>
-      <Frame_c />
+      <Frame_d />
     </View>
   );
 }
---
+++ component/Frame_d/frame_d.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Component } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+const Frame_d = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text>LOGIN</Text>
+            </View>
+            <View style={styles.main}>
+                <TextInput placeholder="Nhập Email"></TextInput>
+                <TextInput placeholder="Nhập mật khẩu"></TextInput>
+                <Button title="LOGIN">LOGIN</Button>
+            </View>
+            <View style={styles.center}>
+                <Text>When you agree too terms and condisstions</Text>
+                <Text>For your password </Text>
+                <Text>Or login with </Text>
+            </View>
+            <View style={styles.footer}>
+                <Button title="Facebook">Facebook</Button>
+                <Button title="ZALO">ZALO</Button>
+                <Button title="GOOGLE">GOOGLE</Button>
+            </View>
+        </View>
+    )
+}
+export default Frame_d
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF'
+    }
+})
